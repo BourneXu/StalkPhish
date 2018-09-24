0b03bd5dd4981dba6149478fd55248c783e27c2e
Chao <xuchao0245@gmail.com>
2018-09-24 19:27:35 -0400

    Add retrieving all possible path

--- stalkphish/tools/download.py
+++ stalkphish/tools/download.py
@@ -14,6 +14,8 @@ import socket
 import zipfile
 import warnings
 import sqlite3
+import redis
+from bs4 import BeautifulSoup
 #from os.path import dirname
 from lxml import html, etree
 from urllib.parse import urlparse
@@ -92,10 +94,63 @@ def TryPKDownload(siteURL,siteDomain,IPaddress,TABLEname,InvTABLEname,DLDir,SQL,
 				err = sys.exc_info()
 				LOG.error("Get PageTitle Error: " +siteURL+ str(err))
 
+			# Set redis to record URL Done list
+			redis_set = redis.Redis(db=1)
+			redis_set.sadd('StalkPhishURLs', 0)
+
+			# Try to retrieve all possible path for one url and find whether there are .zip files
+			try:
+				if len(ziplist) >= 1:
+					for url in ziplist:
+						if redis_set.sismember('StalkPhishURLs', url):
+							continue
+						LOG.info("Retrieving Path "+ url)
+						urllist = RetriveIndexPath(url, proxies, user_agent, [])
+						redis_set.sadd('StalkPhishURLs', *urllist, url)
+						for urlzip in urllist:
+							LOG.info("trying "+ urlzip)
+							rz = requests.get(urlzip, headers=user_agent, proxies=proxies, allow_redirects=True, timeout=(5,12), verify=False)
+							if str(rz.status_code) != "404":
+								lastHTTPcode = str(rz.status_code)
+								zzip = urlzip.replace('/', '_').replace(':', '')
+								try:
+									if "application/zip" in rz.headers['content-type'] or "application/octet-stream" in rz.headers['content-type']:
+										savefile=DLDir+zzip
+										# Still collected file
+										if os.path.exists(savefile):
+											LOG.info("[DL ] Found still collected archive: "+savefile)
+											return
+										# New file to download
+										else:
+											LOG.info("[DL ] Found archive, downloaded it as: "+savefile)
+											with open(savefile, "wb") as code:
+												code.write(rz.content)
+												pass
+											ZipFileName = str(zzip)
+											ZipFileHash = SHA.hashFile(savefile)
+											SQL.SQLiteInvestigUpdatePK(InvTABLEname,siteURL,ZipFileName,ZipFileHash,now,lastHTTPcode)
+											return
+									else:
+										pass
+								except requests.exceptions.ContentDecodingError:
+									LOG.error("[DL ] content-type error")
+								except:
+									pass
+								# 404
+							else:
+								pass
+			except:
+				err = sys.exc_info()
+				LOG.error("DL Error: " + str(err))
+
 			try:
 				# Try too find and download phishing kit archive (.zip)
 				if len(ziplist) >= 1:
 					for zip in ziplist:
+						if redis_set.sismember('StalkPhishURLs', zip+".zip"):
+							continue
+						else:
+							redis_set.sadd('StalkPhishURLs', zip+".zip")
 						if ('=' or '%' or '?' or '-' or '@') not in os.path.basename(os.path.normpath(zip)):
 							try:
 								LOG.info("trying "+zip+".zip")
@@ -151,6 +206,7 @@ def TryPKDownload(siteURL,siteDomain,IPaddress,TABLEname,InvTABLEname,DLDir,SQL,
 				err = sys.exc_info()
 				LOG.error("DL Error: " + str(err))
 
+
 		else:
 			LOG.debug("["+str(r.status_code)+"] "+r.url)
 			lastHTTPcode = str(r.status_code)
@@ -186,3 +242,30 @@ def TryPKDownload(siteURL,siteDomain,IPaddress,TABLEname,InvTABLEname,DLDir,SQL,
 	except:
 		err = sys.exc_info()
 		LOG.error("Error: " + str(err))
+
+
+def RetriveIndexPath(url, proxies, headers, urllist=[]):
+	domin = '/'.join(url.split('/')[0:3])
+	try:
+		r = requests.get(url, proxies=proxies, headers=headers, allow_redirects=True, timeout=(5,12), verify=False)
+	except:
+		pass
+	else:
+		soup = BeautifulSoup(r.text, 'html.parser')
+		if 'Index of' in soup.title.text:
+			# Could be tracked
+			res = soup.select('a')
+			urllist += [url + x['href'] for x in res if '.zip' in x.text]
+			urllist += [domin + x['href'] for x in res if '.zip' in x.text]
+			folderlist = [url + x['href'] for x in res if (x['href'][-1] == '/') and (x.text != 'Parent Directory')]
+			if len(folderlist) > 0:
+				for folderUrl in folderlist:
+					urllist = RetriveIndexPath(folderUrl, proxies, headers, urllist)
+	return list(set(urllist))
+
+
+
+
+
+
+
